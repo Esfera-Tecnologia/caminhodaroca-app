67edab4c4c671bae5f122e72dad1e1a4a84347fb
Ryan W. Fonseca <ryanfonseca183@gmail.com>
2025-08-18 17:31:42 -0300

    Fluxo de recuperação de senha

--- app/(auth)/forgot-password.tsx
+++ app/(auth)/forgot-password.tsx
@@ -3,28 +3,31 @@ import Card from '@/components/Card';
 import Input from '@/components/controls/Input';
 import InputGroup from '@/components/controls/InputGroup';
 import PrimaryButton from '@/components/PrimaryButton';
+import { theme } from '@/theme';
 import { Link } from 'expo-router';
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 export default function Login() {
   return (
-    <AuthContainer title="Entrar com Login" withBackButton={true}>
+    <AuthContainer title="Recuperar senha" withBackButton={true}>
       <Card>
+        <Text style={styles.description}>Informe o e-mail da conta cadastrada através do campo abaixo</Text>
         <InputGroup label="E-mail">
           <Input placeholder="Digite seu e-mail" />
         </InputGroup>
-        <InputGroup label="Senha">
-          <Input placeholder="Digite sua senha"/>
-        </InputGroup>
-        <PrimaryButton label="Entrar" style={{marginBottom: 12}}/>
-        <Link href="/forgot-password" style={styles.link}>Esqueci minha senha</Link>
+        <PrimaryButton label="Enviar link" style={{marginBottom: 12}}/>
+        <Link href="/" style={styles.link}>Voltar para tela de login</Link>
       </Card>
     </AuthContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  description: {
+    color: theme.colors.secondary,
+    marginBottom: 8,
+  },
   link: {
     color: "rgba(33, 37, 41, 0.75)",
     fontSize: 14,
