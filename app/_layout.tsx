936d0250067f1d984d9c248d0e2abdad5ab716c5
Ryan W. Fonseca <ryanfonseca183@gmail.com>
2025-09-19 16:12:23 -0300

    Removendo SafeAreaView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,13 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { Slot } from "expo-router";
 import { KeyboardAvoidingView, Platform } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import ToastManager from 'toastify-react-native';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
-        <SafeAreaView style={{ flex: 1 }}>
-          <Slot />
-        </SafeAreaView>
+        <Slot />
       </KeyboardAvoidingView>
       <ToastManager useModal={false} />
     </AuthProvider>
