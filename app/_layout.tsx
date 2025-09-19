1a9c25994f665dfbe37935240b39d3fcd248f1a0
Ryan W. Fonseca <ryanfonseca183@gmail.com>
2025-09-19 14:19:37 -0300

    Adicionando KeyboardAvoindingView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { Slot } from "expo-router";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import ToastManager from 'toastify-react-native';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Slot />
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+        <Slot />
+      </KeyboardAvoidingView>
       <ToastManager useModal={false} />
     </AuthProvider>
   );
