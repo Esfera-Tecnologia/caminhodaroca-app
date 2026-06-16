6aad2f8df91146cd5def6bf66c0bdb6e42e10e99
Ryan W. Fonseca <ryanfonseca183@gmail.com>
2026-06-16 00:47:06 -0300

    Ajustando espaçamento do menu no android

--- components/controls/Select.tsx
+++ components/controls/Select.tsx
@@ -1,7 +1,8 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome6";
-import { StyleSheet, View, ViewStyle } from "react-native";
+import { Platform, StyleSheet, View, ViewStyle } from "react-native";
 import Dropdown from "react-native-input-select";
 import { DropdownProps } from "react-native-input-select/lib/typescript/src/types/index.types";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export interface SelectProps extends DropdownProps {
   containerStyle?: ViewStyle;
@@ -15,9 +16,22 @@ export default function Select({
   IconName,
   IconStyle,
   emptyListMessage,
+  modalControls,
   ...props
 }: SelectProps) {
-  const groupedStyles = { ...styles.dropdownStyle, ...props.containerStyle, ...props.disabled ? {backgroundColor: '#e9ecef'} : {} } as ViewStyle;
+  const insets = useSafeAreaInsets();
+  const androidBottomOffset =
+    Platform.OS === "android" ? Math.max(insets.bottom, 24) : 0;
+  const groupedStyles = {
+    ...styles.dropdownStyle,
+    ...props.containerStyle,
+    ...(props.disabled ? { backgroundColor: '#e9ecef' } : {}),
+  } as ViewStyle;
+  const modalOptionsContainerStyle = {
+    marginBottom: androidBottomOffset,
+    ...modalControls?.modalOptionsContainerStyle,
+  } as ViewStyle;
+
   if (IconName) groupedStyles["paddingStart"] = 34;
   return (
     <View>
@@ -56,6 +70,10 @@ export default function Select({
           selectAllText: 'Selecionar todos',
           unselectAllText: 'Remover todos',
           emptyListMessage: emptyListMessage || 'Nenhum registro encontrado...',
+        }}
+        modalControls={{
+          ...modalControls,
+          modalOptionsContainerStyle,
         }}
         {...props}
       />
